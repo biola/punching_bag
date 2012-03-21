16a05d455cc3960b396fa789f0b6b1417d4e68e3
Ryan Hall <ryan.e.hall@biola.edu>
2012-03-21 13:33:43 -0700

    Fixed ordering issue in most_hit and sort_by_popularity methods

--- lib/punching_bag/acts_as_punchable.rb
+++ lib/punching_bag/acts_as_punchable.rb
@@ -2,16 +2,19 @@ module PunchingBag
   module ActiveRecord
   
     module ClassMethods
+
+      # Note: this method will only return items if they have 1 or more hits
       def most_hit(since=nil, limit=5)
         query = self.scoped.joins(:punches).group(:punchable_type, :punchable_id)
         query = query.where('punches.average_time >= ?', since) unless since.nil?
-        query.order('SUM(punches.hits) DESC').limit(limit)
+        query.reorder('SUM(punches.hits) DESC').limit(limit)
       end
 
+      # Note: this method will return all items with 0 or more hits
       def sort_by_popularity(dir='DESC')
         query = self.scoped.joins("LEFT OUTER JOIN punches ON punches.punchable_id = #{self.to_s.tableize}.id AND punches.punchable_type = '#{self.to_s}'")
         query = query.group(:id)
-        query.order("SUM(punches.hits) #{dir}")
+        query.reorder("SUM(punches.hits) #{dir}")
       end
     end
     
