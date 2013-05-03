739ebcf3ba12a5835d2be089a3df68cdb9a48031
Simon Hildebrandt <simonhildebrandt@gmail.com>
2013-05-03 15:16:17 +1000

    Fixed Postgres incompatability

--- lib/punching_bag/acts_as_punchable.rb
+++ lib/punching_bag/acts_as_punchable.rb
@@ -5,14 +5,14 @@ module PunchingBag
 
       # Note: this method will only return items if they have 1 or more hits
       def most_hit(since=nil, limit=5)
-        query = self.scoped.joins(:punches).group(:punchable_type, :punchable_id)
+        query = self.scoped.joins(:punches).group(:punchable_type, :punchable_id, "#{table_name}.#{self.class.primary_key}")
         query = query.where('punches.average_time >= ?', since) unless since.nil?
         query.reorder('SUM(punches.hits) DESC').limit(limit)
       end
 
       # Note: this method will return all items with 0 or more hits
       def sort_by_popularity(dir='DESC')
-        query = self.scoped.joins("LEFT OUTER JOIN punches ON punches.punchable_id = #{self.to_s.tableize}.id AND punches.punchable_type = '#{self.to_s}'")
+        query = self.scoped.joins("LEFT OUTER JOIN punches ON punches.punchable_id = #{table_name}.id AND punches.punchable_type = '#{self.name}'")
         query = query.group(:id)
         query.reorder("SUM(punches.hits) #{dir}")
       end
