c79c3483148b4918bf3f6f327176a0f84a49c2c0
tmartin314 <troy@devatotech.com>
2016-12-15 10:20:58 -0500

    added specs for .most_hit

---
+++ spec/lib/punching_bag/acts_as_punchable_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe PunchingBag do
+
+  let(:request) { instance_double(ActionDispatch::Request, bot?: false) }
+
+  let(:article1) { Article.create title: 'Article 1', content: 'Ding, ding ding... ding. Ding. DING. DING! ' }
+  let(:article2) { Article.create title: 'Article 2', content: 'Ding, ding ding... ding. Ding. DING. DING! ' }
+
+  describe '.most_hit' do
+    it 'finds correct result' do
+      PunchingBag.punch(article1, request)
+      PunchingBag.punch(article2, request)
+      PunchingBag.punch(article2, request)
+      expect(Article.most_hit).to include(article2)
+    end
+  end
+
+end
